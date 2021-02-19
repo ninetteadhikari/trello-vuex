ec30b779437cc56fabe8deca70f99e8ef05f0815
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-02-19 12:56:37 +0100

    refactor: Create mixin for duplicate methods to move taskand column

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -32,8 +32,10 @@
 
 <script>
 import ColumnTask from './ColumnTask'
+import movingTasksAndColumnsMixin from '@/mixins/movingTasksAndColumnsMixin'
 export default {
   components: { ColumnTask },
+  mixins: [ movingTasksAndColumnsMixin ],
   props: {
     column: {
       type: Object,
@@ -49,34 +51,6 @@ export default {
     }
   },
   methods: {
-    moveTask (e, toTasks, toTaskIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      const fromTasks = this.board.columns[fromColumnIndex].tasks
-      const fromTaskIndex = e.dataTransfer.getData('from-task-index')
-
-      this.$store.commit('MOVE_TASK', {
-        fromTasks,
-        fromTaskIndex,
-        toTasks,
-        toTaskIndex
-      })
-    },
-    moveTaskOrColumn (e, toTasks, toColumnIndex, toTaskIndex) {
-      const type = e.dataTransfer.getData('type')
-      if (type === 'task') {
-        this.moveTask(
-          e,
-          toTasks,
-          toTaskIndex !== undefined ? toTaskIndex : toTasks.length
-        )
-      } else {
-        this.moveColumn(e, toColumnIndex)
-      }
-    },
-    moveColumn (e, toColumnIndex) {
-      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
-      this.$store.commit('MOVE_COLUMN', { fromColumnIndex, toColumnIndex })
-    },
     pickupColumn (e, fromColumnIndex) {
       e.dataTransfer.effectAllowed = 'move'
       e.dataTransfer.dropEffect = 'move'
