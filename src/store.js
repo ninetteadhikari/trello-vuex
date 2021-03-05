046ca59c924824acda3748cb930d8967d198da4f
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-03-05 13:03:26 +0100

    fix: Add try catch for all store actions

--- src/store.js
+++ src/store.js
@@ -15,151 +15,133 @@ export default new Vuex.Store({
   },
   actions: {
     async fetchAllData ({ commit }) {
-      await board
-        .allDocs({ include_docs: true })
-        .then(doc => {
-          commit('SET_BOARD', doc.rows)
-        })
-        .catch(error => {
-          console.log(error)
-        })
+      try {
+        const doc = await board.allDocs({ include_docs: true })
+        commit('SET_BOARD', doc.rows)
+      } catch (error) {
+        console.log(error)
+      }
     },
-    async dbSync ({ dispatch }) {
+    dbSync ({ dispatch }) {
       const opts = { live: true }
-      await board
+      board
         .sync(remoteCouch, opts)
         .on('change', () => {
           dispatch('fetchAllData')
         })
-        .catch(err => console.log(err))
     },
     async addColumn ({ state, commit }, name) {
-      const newPosition = (state.columns.length + 1) / 10
-      const newColumn = {
-        _id: uuidv4(),
-        name,
-        type: 'column',
-        position: newPosition
+      try {
+        const newPosition = (state.columns.length + 1) / 10
+        const newColumn = {
+          _id: uuidv4(),
+          name,
+          type: 'column',
+          position: newPosition
+        }
+        const result = await board.put(newColumn)
+        commit('CREATE_COLUMN', { result, name, newPosition })
+      } catch (error) {
+        console.log(error)
       }
-      await board
-        .put(newColumn)
-        .then(result => {
-          commit('CREATE_COLUMN', { result, name, newPosition })
-        })
-        .catch(error => {
-          console.log(error)
-        })
     },
     async addTask ({ commit }, { name, columnId, filteredTasks }) {
-      const newPosition = (filteredTasks.length + 1) / 10
-      const newTask = {
-        _id: uuidv4(),
-        name,
-        type: 'task',
-        columnId,
-        description: '',
-        position: newPosition
+      try {
+        const newPosition = (filteredTasks.length + 1) / 10
+        const newTask = {
+          _id: uuidv4(),
+          name,
+          type: 'task',
+          columnId,
+          description: '',
+          position: newPosition
+        }
+        const result = await board.put(newTask)
+        commit('CREATE_TASK', { result, name, columnId, newPosition })
+      } catch (error) {
+        console.log(error)
       }
-      await board
-        .put(newTask)
-        .then(result => {
-          commit('CREATE_TASK', { result, name, columnId, newPosition })
-        })
-        .catch(error => {
-          console.log(error)
-        })
     },
     async editTask ({ commit }, { taskId, key, value }) {
-      await board
-        .get(taskId)
-        .then(doc => {
-          doc[key] = value
-          return board.put(doc)
-        })
-        .then(response => {
-          commit('UPDATE_TASK', { taskId, key, value })
-        })
-        .catch(error => {
-          console.log(error)
-        })
+      try {
+        const doc = await board.get(taskId)
+        doc[key] = value
+        const response = await board.put(doc)
+        commit('UPDATE_TASK', { taskId, key, value })
+      } catch (error) {
+        console.log(error)
+      }
     },
     async changeColumnPosition (
       { state, commit },
       { columnId, fromColumnIndex, toColumnIndex }
     ) {
-      let newPosition
-      if (toColumnIndex === 0) {
-        newPosition = state.columns[toColumnIndex].position / 2
-      } else if (toColumnIndex === state.columns.length - 1) {
-        newPosition =
-          (state.columns[toColumnIndex].position + (state.columns.length + 1) / 10) / 2
-      } else if (fromColumnIndex < toColumnIndex) {
-        newPosition =
-        (state.columns[toColumnIndex + 1].position + state.columns[toColumnIndex].position) /
+      try {
+        let newPosition
+        if (toColumnIndex === 0) {
+          newPosition = state.columns[toColumnIndex].position / 2
+        } else if (toColumnIndex === state.columns.length - 1) {
+          newPosition =
+            (state.columns[toColumnIndex].position + (state.columns.length + 1) / 10) / 2
+        } else if (fromColumnIndex < toColumnIndex) {
+          newPosition =
+          (state.columns[toColumnIndex + 1].position + state.columns[toColumnIndex].position) /
           2
-      } else if (fromColumnIndex > toColumnIndex) {
-        newPosition =
-        (state.columns[toColumnIndex - 1].position + state.columns[toColumnIndex].position) /
-        2
-      }
+        } else if (fromColumnIndex > toColumnIndex) {
+          newPosition =
+          (state.columns[toColumnIndex - 1].position + state.columns[toColumnIndex].position) /
+          2
+        }
 
-      await board
-        .get(columnId)
-        .then(doc => {
-          doc.position = newPosition
-          return board.put(doc)
+        const doc = await board.get(columnId)
+        doc.position = newPosition
+        const response = await board.put(doc)
+        commit('MOVE_COLUMN', {
+          fromColumnIndex,
+          newPosition
         })
-        .then(response => {
-          commit('MOVE_COLUMN', {
-            fromColumnIndex,
-            newPosition
-        })
-        })
-        .catch(error => console.log(error))
+      } catch (error) {
+        console.log(error)
+      }
     },
     async changeTaskPosition (
       { state, commit },
       { fromTaskIndex, toTaskIndex, fromTaskId, toTaskColumnId }
     ) {
-      const toTasks = state.tasks.filter(
-        task => toTaskColumnId === task.columnId
-      )
-
-      let newPosition
-      if (toTasks.length === 0) {
-        newPosition = 0.1
-      } else if (toTaskIndex === 0) {
-        newPosition = toTasks[toTaskIndex].position / 2
-      } else if (toTaskIndex === toTasks.length - 1) {
-        newPosition =
-          (toTasks[toTaskIndex].position + (toTasks.length + 1) / 10) / 2
-      } else if (fromTaskIndex < toTaskIndex) {
-        newPosition =
-        (toTasks[toTaskIndex + 1].position + toTasks[toTaskIndex].position) /
+      try {
+        const toTasks = state.tasks.filter(
+          task => toTaskColumnId === task.columnId
+        )
+        let newPosition
+        if (toTasks.length === 0) {
+          newPosition = 0.1
+        } else if (toTaskIndex === 0) {
+          newPosition = toTasks[toTaskIndex].position / 2
+        } else if (toTaskIndex === toTasks.length - 1) {
+          newPosition =
+            (toTasks[toTaskIndex].position + (toTasks.length + 1) / 10) / 2
+        } else if (fromTaskIndex < toTaskIndex) {
+          newPosition =
+          (toTasks[toTaskIndex + 1].position + toTasks[toTaskIndex].position) /
           2
-      } else if (fromTaskIndex > toTaskIndex) {
-        newPosition =
-        (toTasks[toTaskIndex - 1].position + toTasks[toTaskIndex].position) /
-        2
-      }
+        } else if (fromTaskIndex > toTaskIndex) {
+          newPosition =
+          (toTasks[toTaskIndex - 1].position + toTasks[toTaskIndex].position) /
+          2
+        }
 
-      await board
-        .get(fromTaskId)
-        .then(doc => {
-          doc.columnId = toTaskColumnId
-          doc.position = newPosition
-          return board.put(doc)
+        const doc = await board.get(fromTaskId)
+        doc.columnId = toTaskColumnId
+        doc.position = newPosition
+        const response = await board.put(doc)
+        commit('MOVE_TASK', {
+          toTaskColumnId,
+          fromTaskId,
+          newPosition
         })
-        .then(response => {
-          commit('MOVE_TASK', {
-            toTaskColumnId,
-            fromTaskId,
-            newPosition
-          })
-        })
-        .catch(error => {
-          console.log(error)
-        })
+      } catch (error) {
+        console.log(error)
+      }
     }
   },
   mutations: {
@@ -182,6 +164,7 @@ export default new Vuex.Store({
             : state.tasks[taskIndex] = item.doc
         }
       })
+
       state.columns.sort((a, b) => a.position - b.position)
       state.tasks.sort((a, b) => a.position - b.position)
     },
