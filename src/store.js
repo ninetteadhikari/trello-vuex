759122a1dedc96c924501c034b782629867a7921
Ninette Adhikari <ninette.adhikari@gmail.com>
2021-03-11 17:31:16 +0100

    refactor: Refactor adding new position for column and task

--- src/store.js
+++ src/store.js
@@ -93,24 +93,40 @@ export default new Vuex.Store({
     ) {
       try {
         let newPosition
-        if (toColumnIndex === 0) {
-          newPosition = state.columns[toColumnIndex].position / 2
-        } else if (toColumnIndex === state.columns.length - 1) {
-          newPosition =
-            (state.columns[toColumnIndex].position + (state.columns.length + 1) / 10) / 2
+
+        const toColumn = state.columns[toColumnIndex]
+        const toColumnPosition = toColumn.position
+        const columnLength = state.columns.length
+
+        const isColumnIndexFirst = toColumnIndex === 0
+        const isColumnIndexLast = toColumnIndex === columnLength - 1
+
+        /* NEW POSITION
+        The new position of the column that is moved, is the median of the position
+        of the two surrounding columns. */
+
+        if (isColumnIndexFirst) {
+          // Scenario 1: When a column is moved to the first position
+          newPosition = toColumnPosition / 2
+        } else if (isColumnIndexLast) {
+          // Scenario 2: When a column is moved to the last position
+          const nextToLastColumnPosition = (columnLength + 1) / 10
+          newPosition = (toColumnPosition + nextToLastColumnPosition) / 2
         } else if (fromColumnIndex < toColumnIndex) {
-          newPosition =
-          (state.columns[toColumnIndex + 1].position + state.columns[toColumnIndex].position) /
-          2
+          // Scenario 3: When a column is moved from left to right
+          const nextColumn = state.columns[toColumnIndex + 1]
+          const nextColumnPosition = nextColumn.position
+          newPosition = (nextColumnPosition + toColumnPosition) / 2
         } else if (fromColumnIndex > toColumnIndex) {
-          newPosition =
-          (state.columns[toColumnIndex - 1].position + state.columns[toColumnIndex].position) /
-          2
+          // Scenario 4: When a column is moved from right to left
+          const previousColumn = state.columns[toColumnIndex - 1]
+          const previousColumnPosition = previousColumn.position
+          newPosition = (previousColumnPosition + toColumnPosition) / 2
         }
 
         const doc = await board.get(columnId)
         doc.position = newPosition
-        const response = await board.put(doc)
+        await board.put(doc)
         commit('MOVE_COLUMN', {
           fromColumnIndex,
           newPosition
@@ -124,31 +140,49 @@ export default new Vuex.Store({
       { fromTaskIndex, toTaskIndex, fromTaskId, toTaskColumnId }
     ) {
       try {
-        const toTasks = state.tasks.filter(
+        let newPosition
+
+        const toTasksList = state.tasks.filter(
           task => toTaskColumnId === task.columnId
         )
-        let newPosition
-        if (toTasks.length === 0) {
+        const toTask = toTasksList[toTaskIndex]
+        const toTaskPosition = toTask.position
+        const taskLength = toTasksList.length
+
+        const isColumnEmpty = taskLength === 0
+        const isTaskIndexFirst = toTaskIndex === 0
+        const isTaskIndexLast = toTaskIndex === taskLength - 1
+
+        /* NEW POSITION
+        The new position of the task that is moved, is the median of the position
+        of the two surrounding tasks. */
+
+        if (isColumnEmpty) {
+          // Scenario 1: When a task is moved to an empty column
           newPosition = 0.1
-        } else if (toTaskIndex === 0) {
-          newPosition = toTasks[toTaskIndex].position / 2
-        } else if (toTaskIndex === toTasks.length - 1) {
-          newPosition =
-            (toTasks[toTaskIndex].position + (toTasks.length + 1) / 10) / 2
+        } else if (isTaskIndexFirst) {
+          // Scenario 2: When a task is moved to the first position
+          newPosition = toTaskPosition / 2
+        } else if (isTaskIndexLast) {
+          // Scenario 3: When a task is moved to the last position
+          const nextToLastTaskPosition = (taskLength + 1) / 10
+          newPosition = (toTaskPosition + nextToLastTaskPosition) / 2
         } else if (fromTaskIndex < toTaskIndex) {
-          newPosition =
-          (toTasks[toTaskIndex + 1].position + toTasks[toTaskIndex].position) /
-          2
+          // Scenario 4: When a task is moved from top to bottom
+          const nextTask = toTasksList[toTaskIndex + 1]
+          const nextTaskPosition = nextTask.position
+          newPosition = (nextTaskPosition + toTaskPosition) / 2
         } else if (fromTaskIndex > toTaskIndex) {
-          newPosition =
-          (toTasks[toTaskIndex - 1].position + toTasks[toTaskIndex].position) /
-          2
+          // Scenario 5: When a task is moved from bottom to top
+          const previousTask = toTasksList[toTaskIndex - 1]
+          const previousTaskPosition = previousTask.position
+          newPosition = (previousTaskPosition + toTaskPosition) / 2
         }
 
         const doc = await board.get(fromTaskId)
         doc.columnId = toTaskColumnId
         doc.position = newPosition
-        const response = await board.put(doc)
+        await board.put(doc)
         commit('MOVE_TASK', {
           toTaskColumnId,
           fromTaskId,
@@ -169,7 +203,7 @@ export default new Vuex.Store({
           column => column._id === item.doc._id
         )
             if (columnIndex === -1) {
-              // Add new column
+              // Add new column if column doesn't exist
               state.columns.push(item.doc)
             } else {
               // Replace existing column
